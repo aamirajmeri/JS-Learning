49d17763dc8bfb49b058ec93af14ba84b732171a
aamirajmeri <aamirajmeri7878@gmail.com>
2024-01-26 14:33:48 +0545

    Rivision Day

---
+++ Day-10/script.js
@@ -0,0 +1,125 @@
+// let userNumber = prompt("Enter your name");
+
+// if(userNumber % 2 == 0){
+//     alert("your number " + userNumber + " is even");
+// }else{
+//     alert("your number " + userNumber + " is odd");
+// }
+
+// for(let i = 1; i <= userNumber ; i++){
+//     console.log(i);
+    
+// }
+
+// for(let i = 1; i<=userNumber ; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// for(let i = 1; i<=userNumber; i++){
+//     if(i % 5 == 0){
+//         console.log(i);
+        
+//     }
+// }
+
+// let num1 = prompt("Enter number1");
+// let num2 = prompt("Enter number2");
+
+// function addNumbers(num1,num2){
+//     return num1 + num2;
+// }
+
+// let sum = addNumbers(num1,num2);
+
+// console.log(sum);
+
+
+
+// let num1 = prompt("Enter your 1st Number");
+// let num2 = prompt("Enter your 2nd Number");
+
+// function mulNumber(num1,num2)
+// {
+//     return num1 * num2;
+// }
+
+// let mulNum1 = mulNumber(num1,num2);
+
+// console.log(mulNum1);
+
+
+//  let num1 = prompt("Enter your 1st number");
+//  let num2 = prompt("Enter your 2nd number");
+//  let num3 = prompt("Enter your 3rd number");
+
+//  function add3Num(num1,num2,num3){
+//     return num1 * num2 * num3;
+//  }
+
+//  let sum = add3Num(num1,num2,num3);
+
+//  document.write(sum);
+ 
+
+// take users age , check weather it's adult or not. if it's adult then print you are invited to party else print go & drink your milk .
+
+// display sum of all odd numbers from 1 to 30. //for loop.
+
+// display all numbers which are divisible 5 as "Five" from 1 - 100;
+
+
+//  let age = prompt("Enter your age");
+// age = 18;
+// let i = age;
+
+//     if(let i=1; i<=18; i++){
+//         console.log("You are Invited to our party ");
+//     }
+//      else (i<18){
+//         console.log("go & drink your milk");  
+//     }
+
+// console.log(i);
+
+
+let sum = 0;
+
+for(i = 1; i<=30; i++)
+{
+    if(i % 2 == 0){
+        sum = sum + i;
+    }
+   
+}
+
+console.log(sum);
+
+
+
+//  for(i=1; i<=30; i++)
+// {
+//     if(i % 2 == 0){
+//         console.log("sum of ",even );
+ 
+//     }
+//     else {
+//         console.log("sum of ",odd );
+//     }
+// }
+
+// console.log(i);
+
+
+// if( i=1; i <= 18; i++)
+// {
+//     console.log("You are Invited to our party ");
+// }else if(){
+//     console.log("go & drink your milk");   
+// }
+
+// console.log(i);
+
+
