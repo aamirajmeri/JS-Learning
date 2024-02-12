6a2dfffc6dbce2b8d0b649db180ccc3224beb863
aamirajmeri <aamirajmeri7878@gmail.com>
2024-02-12 20:28:29 +0545

    random color generator is added

---
+++ Day-28/script.js
@@ -0,0 +1,18 @@
+const btnEl = document.querySelector(".btn-generate");
+const backgroundEl = document.querySelector(".main-container")
+
+// hashCode = hashCode.toSubstring(2,8); 
+// hashCode = "#" + hashCode;
+
+// console.log(hashCode);
+
+// let mystring = "4793582146";
+// console.log(mystring.substring(0,3));
+
+
+btnEl.addEventListener("click",()=>{
+    let hashCode = "#" + Math.random().toString(16).substring(2,8);
+    btnEl.innerHTML = hashCode; 
+    backgroundEl.style.backgroundColor = hashCode;
+})
+
